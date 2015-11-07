bd018f74d477e215c9b3be2c4889507222e31171
Alexander Cederblad <alexcederblad@gmail.com>
2015-11-07 19:29:23 +0100

    Fix some Mac compat issues

--- gtk/install.sh
+++ gtk/install.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 set -e
 
 dir=$(dirname $0)
 
 # Copy gtk.css
-echo "Copying gtk.css"
-cp $dir/gtk.css ~/.config/gtk-3.0/gtk.css
+gtk_path="~/.config/gtk-3.0"
+if [ -d gtk_path ];
+then
+    echo "Copying gtk.css"
+    cp $dir/gtk.css $gtk_path/gtk.css
+    echo "gtk setup done!"
+fi
 
-echo "gtk setup done!"
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 set -e
 
 echo "Running all install.sh scripts."
-find -mindepth 2 -name "install.sh" | while read filename; do
+find ./**/install.sh | while read filename; do
     echo "\nRunning $filename:"
     sh $filename
 done

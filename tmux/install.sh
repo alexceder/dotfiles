a9dd2433e2d570dc6e83d9bc7cadf0a1c3884adc
Alexander Cederblad <alexcederblad@gmail.com>
2020-08-13 11:01:49 +0200

    Add OS specific configurations to tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,4 +8,23 @@ dir=$(dirname "$0")
 echo "Copying .tmux.conf"
 cp "$dir"/.tmux.conf ~/.tmux.conf
 
+# Append OS specific configuration.
+uname="$(uname -s)"
+case "$uname" in
+  Linux*)  os=linux;;
+  Darwin*) os=macos;;
+  *)       os="UNKNOWN:${uname}"
+esac
+os_file=".tmux.${os}.conf"
+if [ -f "$dir/$os_file" ]; then
+  echo "Appending ${os_file} to ~/.tmux.conf"
+  {
+    echo
+    echo "# Following lines appended from $os_file"
+  } >> ~/.tmux.conf
+  cat "$dir/$os_file" >> ~/.tmux.conf
+else
+  echo "Either OS ${os} is not valid or ${os_file} does not exist"
+fi
+
 echo "tmux setup done!"

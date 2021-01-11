71d28b01a03d6375edfc19b1f850863a7d84f066
Alexander Cederblad <alexcederblad@gmail.com>
2021-01-11 10:11:29 +0100

    Make macOS-specific alacritty setup only happen on macOS
    Seems like the configuration that was there for macOS was invalid for my
    Ubuntu setup. I was hoping it would just not work when not present for
    the OS. And not sure there is a way to conditionally set it in YAML, so
    copying the way I did it in the tmux setup.

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -9,4 +9,22 @@ echo "Copying alacritty.yml"
 mkdir -p ~/.config/alacritty
 cp "$dir"/alacritty.yml ~/.config/alacritty
 
+uname="$(uname -s)"
+case "$uname" in
+  Linux*)  os=linux;;
+  Darwin*) os=macos;;
+  *)       os="UNKNOWN:${uname}"
+esac
+os_file="alacritty.${os}.yml"
+if [ -f "$dir/$os_file" ]; then
+  echo "Appending ${os_file} to ~/.config/alacritty/alacritty.conf"
+  {
+    echo
+    echo "# Following lines appended from $os_file"
+  } >> ~/.config/alacritty/alacritty.yml
+  cat "$dir/$os_file" >> ~/.config/alacritty/alacritty.yml
+else
+  echo "Either OS ${os} is not valid or ${os_file} does not exist"
+fi
+
 echo "Alacritty setup done!"

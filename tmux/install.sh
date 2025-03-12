7a62dfcdc9c9b099b949fc22e5f1c63a91a5d7cd
Alexander Cederblad <alexcederblad@gmail.com>
2025-03-12 13:51:37 +0100

    Update tmux config
    Move tmux into ~/.config/tmux directory.
    
    Also some changes to the actual config, the nicest thing was to add:
    
    set -sg escape-time 0
    
    Which makes vim a bit faster, since we are not waiting for escape when
    doing visual block, etc. However, it does make it so that I cannot use
    Meta then ESC-2 to make the splits equal in size, need to figure out
    something for that, but I appreciate this better than the alternative
    for now.

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,9 +4,10 @@ set -e
 
 dir=$(dirname "$0")
 
-# Copy .tmux.conf
-echo "Copying .tmux.conf"
-cp "$dir"/.tmux.conf ~/.tmux.conf
+# Copy tmux.conf
+echo "Copying tmux.conf"
+mkdir -p ~/.config/tmux
+cp "$dir"/tmux.conf ~/.config/tmux.conf
 
 # Append OS specific configuration.
 uname="$(uname -s)"
@@ -15,14 +16,14 @@ case "$uname" in
   Darwin*) os=macos;;
   *)       os="UNKNOWN:${uname}"
 esac
-os_file=".tmux.${os}.conf"
+os_file="tmux.${os}.conf"
 if [ -f "$dir/$os_file" ]; then
-  echo "Appending ${os_file} to ~/.tmux.conf"
+  echo "Appending ${os_file} to ~/.config/tmux.conf"
   {
     echo
     echo "# Following lines appended from $os_file"
-  } >> ~/.tmux.conf
-  cat "$dir/$os_file" >> ~/.tmux.conf
+  } >> ~/.config/tmux.conf
+  cat "$dir/$os_file" >> ~/.config/tmux.conf
 else
   echo "Either OS ${os} is not valid or ${os_file} does not exist"
 fi

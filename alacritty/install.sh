58fbd2edf0a84035c9ec751da8be871216073c74
Alexander Cederblad <alexcederblad@gmail.com>
2025-03-12 14:00:03 +0100

    Update alacritty config
    Moving to toml from yaml.
    
    Also changes the font and colors.

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -4,10 +4,10 @@ set -e
 
 dir=$(dirname "$0")
 
-# Copy alacritty.yml
-echo "Copying alacritty.yml"
+# Copy alacritty.toml
+echo "Copying alacritty.toml"
 mkdir -p ~/.config/alacritty
-cp "$dir"/alacritty.yml ~/.config/alacritty
+cp "$dir"/alacritty.toml ~/.config/alacritty
 
 uname="$(uname -s)"
 case "$uname" in
@@ -15,16 +15,16 @@ case "$uname" in
   Darwin*) os=macos;;
   *)       os="UNKNOWN:${uname}"
 esac
-os_file="alacritty.${os}.yml"
+os_file="alacritty.${os}.toml"
 if [ -f "$dir/$os_file" ]; then
-  echo "Appending ${os_file} to ~/.config/alacritty/alacritty.conf"
+  echo "Appending ${os_file} to ~/.config/alacritty/alacritty.toml"
   {
     echo
     echo "# Following lines appended from $os_file"
-  } >> ~/.config/alacritty/alacritty.yml
-  cat "$dir/$os_file" >> ~/.config/alacritty/alacritty.yml
+  } >> ~/.config/alacritty/alacritty.toml
+  cat "$dir/$os_file" >> ~/.config/alacritty/alacritty.toml
 else
   echo "Either OS ${os} is not valid or ${os_file} does not exist"
 fi
 
-echo "Alacritty setup done!"
+echo "alacritty setup done!"
